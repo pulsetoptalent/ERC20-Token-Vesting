ee4ffb4550db6be7f68ac76eff1d72ae5cdad389
Del-Sarto <aiperdelsarto@gmail.com>
2023-11-21 02:47:39 +0300

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,119 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-abi-exporter");
+require("hardhat-docgen");
+require("hardhat-tracer");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-preprocessor");
+const fs = require("fs");
+
+const etherscanApiKey = getEtherscanApiKey();
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => line.trim().split("="));
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+    cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
+  },
+  // This fully resolves paths for imports in the ./lib directory for Hardhat
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+  networks: {
+    mainnet: mainnetNetworkConfig(),
+    goerli: goerliNetworkConfig(),
+  },
+  abiExporter: {
+    path: "./build/abi",
+    clear: true,
+    flat: true,
+    spacing: 2,
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  },
+  gasReporter: {
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: `${etherscanApiKey}`,
+  },
+};
+
+function mainnetNetworkConfig() {
+  let url = "https://mainnet.infura.io/v3/";
+  let accountPrivateKey =
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+  if (process.env.MAINNET_ENDPOINT) {
+    url = `${process.env.MAINNET_ENDPOINT}`;
+  }
+
+  if (process.env.MAINNET_PRIVATE_KEY) {
+    accountPrivateKey = `${process.env.MAINNET_PRIVATE_KEY}`;
+  }
+
+  return {
+    url: url,
+    accounts: [accountPrivateKey],
+  };
+}
+
+function goerliNetworkConfig() {
+  let url = "https://goerli.infura.io/v3/";
+  let accountPrivateKey =
+    "0x0000000000000000000000000000000000000000000000000000000000000000";
+  if (process.env.GOERLI_ENDPOINT) {
+    url = `${process.env.GOERLI_ENDPOINT}`;
+  }
+
+  if (process.env.GOERLI_PRIVATE_KEY) {
+    accountPrivateKey = `${process.env.GOERLI_PRIVATE_KEY}`;
+  }
+
+  return {
+    url: url,
+    accounts: [accountPrivateKey],
+  };
+}
+
+function getEtherscanApiKey() {
+  let apiKey = "";
+  if (process.env.ETHERSCAN_API_KEY) {
+    apiKey = `${process.env.ETHERSCAN_API_KEY}`;
+  }
+  return apiKey;
+}
